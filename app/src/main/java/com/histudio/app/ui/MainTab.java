044b2cb9f895b3adbb12037a1edb60eacfe553e4
237007180@qq.com <laolin123>
2019-10-31 16:12:15 +0800

    优化

--- app/src/main/java/com/histudio/app/ui/MainTab.java
+++ app/src/main/java/com/histudio/app/ui/MainTab.java
@@ -1,14 +1,15 @@
 package com.histudio.app.ui;
 
 
+import com.histudio.app.R;
 import com.histudio.app.fragment.MainTainFragment;
 import com.histudio.app.fragment.OtherFragment;
 
 public enum MainTab {
 
-    RECORD(0, com.histudio.app.R.string.title_frame_main, com.histudio.app.R.drawable.radio_activity_selector, MainTainFragment.class),
-    CLUB(1, com.histudio.app.R.string.title_frame_teach, com.histudio.app.R.drawable.radio_club_selector, MainTainFragment.class),
-    FIND(2, com.histudio.app.R.string.title_frame_class, com.histudio.app.R.drawable.radio_find_selector, OtherFragment.class);
+    RECORD(0, com.histudio.app.R.string.title_frame_main, R.drawable.radio_activity_selector, MainTainFragment.class),
+    CLUB(1, com.histudio.app.R.string.title_frame_teach, R.drawable.radio_club_selector, MainTainFragment.class),
+    FIND(2, com.histudio.app.R.string.title_frame_class, R.drawable.radio_find_selector, OtherFragment.class);
 
     private int idx;
     private int resName;
